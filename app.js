49b902ba16124ae605a4d5e41d5d2556906e5ba6
Aakash <ahanda205@gmail.com>
2021-01-30 17:11:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.get('/rest',(req,res) => {
 
 //MealType Route
 app.get('/meal',(req,res) => {
-  db.collection('mealType').find().toArray((err,result) => {
+  db.collection('mealtype').find().toArray((err,result) => {
     if(err) throw err;
     res.send(result)
   })
